fc61be4b43e11a8b42b5e0de8268131019219744
pcondessa <pedrocondessa2008@hotmail.com>
2022-06-29 15:30:51 -0300

    done data_science

--- src/titanic_ml_project/helpers/helper_functions.py
+++ src/titanic_ml_project/helpers/helper_functions.py
@@ -1,5 +1,15 @@
 import re
-import pandas as pd
+from importlib import import_module
+
+def model_from_string(
+    model_name
+):
+    return getattr(
+        import_module(
+            ('.').join(model_name.split('.')[:-1])
+        ),
+        model_name.rsplit(".")[-1],
+    )()
 
 def get_title(
     name: str
